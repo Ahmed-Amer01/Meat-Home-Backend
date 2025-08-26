d09e4c82807deb84cdc03bd17d0084f6b8992850
Ahmed-Amer01 <ahmedamer42214953@gmail.com>
2025-08-26 20:53:13 +0300

    enhance user entity

--- src/main/java/com/example/meat_home/entity/Customer.java
+++ src/main/java/com/example/meat_home/entity/Customer.java
@@ -18,6 +18,7 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     @Column(unique = true, nullable = false)

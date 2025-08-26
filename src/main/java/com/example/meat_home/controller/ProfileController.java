cc1cafe040e48408fbb0b70e8a6a9e9d0da38c58
Ahmed-Amer01 <ahmedamer42214953@gmail.com>
2025-08-26 18:03:32 +0300

    add PreAuth in profile controller

--- src/main/java/com/example/meat_home/controller/ProfileController.java
+++ src/main/java/com/example/meat_home/controller/ProfileController.java
@@ -6,6 +6,7 @@ import com.example.meat_home.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,6 +17,7 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @PatchMapping("/update")
+    @PreAuthorize("hasAnyRole('ADMIN','CALLCENTER','DELIVERY','CUSTOMER')")
     public ResponseEntity<?> updateProfile(
             @RequestHeader("Authorization") String authHeader,
             @RequestBody UpdateProfileRequest request
